4c9e4e012406e0ee8397ff222b3617fd36de3404
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-24 12:16:25 +0530

    predict the type of fruit given these values

--- fruitClassification.py
+++ fruitClassification.py
@@ -1,5 +1,6 @@
 from sklearn import tree
 
+
 features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
 labels = ["apple", "apple", "orange", "orange"]
 
@@ -10,6 +11,14 @@ clf = tree.DecisionTreeClassifier()
 # Find patterns in the training data
 # Training algorithm is included in the classifier object is called Fit
 clf = clf.fit(features, labels)
+# clf is now a trained classifier
+
+
+# Classifier notice oranges tends to weigh more, so it'll crate a rule that the heavier
+# fruit is, more likely it is to be orange
+
+prediction = clf.predict([150, "bumpy"])
+
 
 
 
