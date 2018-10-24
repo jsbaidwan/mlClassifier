7d7e7b28ecebf92a58d90d7c54480eb67da7aa29
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-24 11:59:31 +0530

    create classifier for supervised learning

--- fruitClassification.py
+++ fruitClassification.py
@@ -1,5 +1,7 @@
-from sklear import tree
+from sklearn import tree
 
 features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
 labels = ["apple", "apple", "orange", "orange"]
 
+# Classifier for supervised learning
+clf = tree.DecisionTreeClassifier()
