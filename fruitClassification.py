28d0ce0c29b550a2e141b3fd68f47900ed79e30e
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-24 12:28:50 +0530

    print the output

--- fruitClassification.py
+++ fruitClassification.py
@@ -1,15 +1,18 @@
 from sklearn import tree
 
+# Store are list in the features variable consists of [weight, texture]
+features = [[140, 0], [130, 0], [150, 1], [170, 1]]
 
-features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
 labels = ["apple", "apple", "orange", "orange"]
 
 # Classifier for supervised learning
 # Classifier is a box of rules
+# clf will store our DecisionTree classifier
 clf = tree.DecisionTreeClassifier()
 
 # Find patterns in the training data
 # Training algorithm is included in the classifier object is called Fit
+# Fit method trains the decision tree on our data set
 clf = clf.fit(features, labels)
 # clf is now a trained classifier
 
@@ -17,7 +20,12 @@ clf = clf.fit(features, labels)
 # Classifier notice oranges tends to weigh more, so it'll crate a rule that the heavier
 # fruit is, more likely it is to be orange
 
-prediction = clf.predict([150, "bumpy"])
+prediction = clf.predict([[150, 0]])
+
+# print the output
+print(prediction)
+
+
 
 
 
