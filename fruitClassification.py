0b124f73dda577177b047ed397d0dfacbbac7233
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-24 12:07:17 +0530

    Find patterns in the trained algorithm

--- fruitClassification.py
+++ fruitClassification.py
@@ -4,4 +4,14 @@ features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
 labels = ["apple", "apple", "orange", "orange"]
 
 # Classifier for supervised learning
+# Classifier is a box of rules
 clf = tree.DecisionTreeClassifier()
+
+# Find patterns in the training data
+# Training algorithm is included in the classifier object is called Fit
+clf = clf.fit(features, labels)
+
+
+
+
+
