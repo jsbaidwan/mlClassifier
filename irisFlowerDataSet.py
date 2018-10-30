72b66abf21c36a07c85babbd14563a5cdf8d52a4
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-30 14:27:44 +0530

    storing first entry of each flower to test it later on

--- irisFlowerDataSet.py
+++ irisFlowerDataSet.py
@@ -4,20 +4,6 @@ from sklearn.datasets import load_iris
 
 iris = load_iris()
 
-# features
-print(iris.feature_names)
-
-# labels
-print(iris.target_names)
-
-# first entry in the iris data set
-print(iris.data[0])
-
-#
-print(iris.target[0])
-
-# print entire Iris dataset
-for i in range(len(iris.target)):
-    print("Example %d: label %s, feature %s" % (i, iris.target[i], iris.data[i]))
-
+# Remove one example of each type of flower by first storing it in test_index variable
+test_index = [0, 50, 100]
 
