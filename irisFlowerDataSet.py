3980d0a79f7e305c692bdb46a3b5c99895387999
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-10-30 11:59:52 +0530

    Print entire Iris Data set

--- irisFlowerDataSet.py
+++ irisFlowerDataSet.py
@@ -13,4 +13,11 @@ print(iris.target_names)
 # first entry in the iris data set
 print(iris.data[0])
 
+#
+print(iris.target[0])
+
+# print entire Iris dataset
+for i in range(len(iris.target)):
+    print("Example %d: label %s, feature %s" % (i, iris.target[i], iris.data[i]))
+
 
