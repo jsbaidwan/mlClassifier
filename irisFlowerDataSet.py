35d7a8ea71430dce7821b0c74496a8e29bb08db7
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-11-05 19:13:09 +0530

    Training Data: removing 3 entries from data and target variables

--- irisFlowerDataSet.py
+++ irisFlowerDataSet.py
@@ -8,3 +8,9 @@ iris = load_iris()
 
 # Remove one example of each type of flower by first storing it in test_index variable
 test_index = [0, 50, 100]
+
+
+# Training data
+# removing three entries from the data and target variables
+train_target = np.delete(iris.target, test_index)
+train_data = np.delete(iris.data, test_index, axis=0)
