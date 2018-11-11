fe25588badec4a095aad308f6a47fdaf3fed62d7
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-11-11 22:38:23 +0530

    view the decision tree in pdf

--- irisFlowerDataSet.py
+++ irisFlowerDataSet.py
@@ -26,3 +26,18 @@ clf.fit(train_data, train_target)
 
 print(test_target)
 print(clf.predict(test_data))
+
+# Viz graph
+from sklearn.externals.six import StringIO
+import pydotplus
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                     out_file=dot_data,
+                     feature_names=iris.feature_names,
+                     class_names=iris.target_names,
+                     filled=True, rounded=True,
+                     impurity=False)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
+
