adbc4bed7e80ebfa6539ed22c48d95bbb57f65a6
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-11-18 23:55:16 +0530

    Import an iris dataset

---
+++ pipline.py
@@ -0,0 +1,11 @@
+
+# How to test a model and determine accuracy
+
+# Partition data into 2 sets, train and test
+
+# import a dataset
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+
