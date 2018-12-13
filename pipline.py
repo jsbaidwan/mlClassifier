f22a360ec376f0df558f1c5caf50a6e0966985db
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-12-13 18:06:14 +0530

    REPLACE: decisiontree classifier with KNeighborsClassifier

--- pipline.py
+++ pipline.py
@@ -19,9 +19,9 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
 # Classifier
-from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 
-my_classifier = tree.DecisionTreeClassifier()
+my_classifier = KNeighborsClassifier()
 my_classifier.fit(X_train, y_train)
 
 # Predict
@@ -32,21 +32,21 @@ print(predictions)
 from sklearn.metrics import accuracy_score
 print(accuracy_score(predictions, y_test))
 
-# Repeat using KNN
-# Classifier
-from sklearn.neighbors import KNeighborsClassifier
-
-my_classifier = KNeighborsClassifier()
-my_classifier.fit(X_train, y_train)
-
-# predict
-predictions = my_classifier.predict(X_test)
-print(predictions)
-
-# test
-from sklearn.metrics import accuracy_score
-
-# print the predication
-print(accuracy_score(y_test, predictions))
+# # Repeat using KNN
+# # Classifier
+# from sklearn.neighbors import KNeighborsClassifier
+#
+# my_classifier = KNeighborsClassifier()
+# my_classifier.fit(X_train, y_train)
+#
+# # predict
+# predictions = my_classifier.predict(X_test)
+# print(predictions)
+#
+# # test
+# from sklearn.metrics import accuracy_score
+#
+# # print the predication
+# print(accuracy_score(predictions, y_test))
 
 
