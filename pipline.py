d8e9ccd5821ff1e2a7f8615b77a31d6e1e332451
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-12-17 12:18:55 +0530

    define the fit method

--- pipline.py
+++ pipline.py
@@ -1,6 +1,7 @@
 class ScrappyKNN():
     def fit(self, X_train, y_train):
-        pass
+        self.X_train = X_train
+        self.y_train - y_train
 
     def predict(self, X_test):
         pass
