f65f43a6f94c8d52b82464c76b2b87b4ad9ac779
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-12-27 16:30:02 +0530

    track the closest point

--- pipline.py
+++ pipline.py
@@ -16,6 +16,9 @@ class ScrappyKNN():
             predictions.append(label)
         return predictions
 
+    def closest(self,row):
+        best_dist = euc(row, self.X_train[0])
+
 # How to test a model and determine accuracy
 
 # Partition data into 2 sets, train and test
